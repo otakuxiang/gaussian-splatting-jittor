37995e3670bac1dcc53f67685429e84cb4029db4
陈浩翔 <chx20@mails.tsinghua.edu.cn>
2024-03-05 15:36:43 +0800

    init

---
+++ gaussian_renderer/diff_gaussian_rasterizater/build/CMakeFiles/CudaRasterizer.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/chx/ssd/3dGS/gaussian_jittor/gaussian_renderer/diff_gaussian_rasterizater/cuda_rasterizer/backward.cu" "CMakeFiles/CudaRasterizer.dir/cuda_rasterizer/backward.cu.o" "gcc" "CMakeFiles/CudaRasterizer.dir/cuda_rasterizer/backward.cu.o.d"
+  "/home/chx/ssd/3dGS/gaussian_jittor/gaussian_renderer/diff_gaussian_rasterizater/cuda_rasterizer/forward.cu" "CMakeFiles/CudaRasterizer.dir/cuda_rasterizer/forward.cu.o" "gcc" "CMakeFiles/CudaRasterizer.dir/cuda_rasterizer/forward.cu.o.d"
+  "/home/chx/ssd/3dGS/gaussian_jittor/gaussian_renderer/diff_gaussian_rasterizater/cuda_rasterizer/rasterizer_impl.cu" "CMakeFiles/CudaRasterizer.dir/cuda_rasterizer/rasterizer_impl.cu.o" "gcc" "CMakeFiles/CudaRasterizer.dir/cuda_rasterizer/rasterizer_impl.cu.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
